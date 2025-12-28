45eff8cff06dc7294c1514274ea397e5e51c473b
Amit K <162456463+AmitKorgaonkar1980@users.noreply.github.com>
2025-12-28 13:29:33 +0530

    Add files via upload

---
+++ PythonProject/Assignment 1 TUTEDUDE.py
@@ -0,0 +1,20 @@
+print('Enter first number: ')
+first = int(input())
+print('Enter second number: ')
+second = int(input())
+
+Result = first + second
+print("Addition = ",Result)
+Result2 = first - second
+print("subtraction = ",Result2)
+Result3 = first * second
+print("multiplication = ",Result3)
+Result4 = first / second
+print("division = ",Result4)
+
+print('Enter your first name: ')
+firstname = input()
+print('Enter your last name: ')
+lastname = input()
+print('Hello',firstname + ' ' + lastname)
+print('Welcome to Python!!')
